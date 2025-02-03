d758bfd548ef9f509c2af4ddeb5dd345277124ea
Alveiro Garcia <alveirogarcia@google.com>
2025-02-03 19:05:37 +0000

    create real cf

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 """General operations"""
+from fastapi import FastAPI
 
+app = FastAPI(debug=True)
+
+@app.post("/process")
 def summing():
     """This method is for sum"""
     return 1+2
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)    
     
